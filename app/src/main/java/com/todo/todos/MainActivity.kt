d1088c1400d1709a2ecaed60cddabfd4e2d1e273
HUILI <root33hash@gmail.com>
2018-07-30 22:20:57 +0900

    display input strings

--- app/src/main/java/com/todo/todos/MainActivity.kt
+++ app/src/main/java/com/todo/todos/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.todo.todos
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        const val EXTRA_MESSAGE: String = "com.todo.MESSAGE"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View?) {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
     }
 }
